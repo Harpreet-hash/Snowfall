fc9165f42eb4f1c3f870b0dd64ff6d5d11a1f3cd
Harpreet-hash <73457305+Harpreet-hash@users.noreply.github.com>
2021-07-31 16:01:54 +0530

    Update snowflake.js

--- snowflake.js
+++ snowflake.js
@@ -6,10 +6,16 @@ class Snowflake {
        density:0.1,
        frictionAir:0.05
    }
-   
+   this.size=random(30,100)
+   this.body=Bodies.circle(x,y,this.size,options);
+   World.add(world,this.body)
+   this.image=loadImage("snow5.webp")
   }
 
  display() {
+
+  imageMode(CENTER)
+  image(this.image,this.body.position.x,this.body.position.y,this.size,this.size)
    
  
     
